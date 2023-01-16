7297f3a18a45545d0cbcd7a0d7145aef3c2474b4
Ival Racha Pratama <ivalivalrakha@gmail.com>
2023-01-16 15:16:59 +0700

    model

---
+++ src/main/java/org/ival/model/JobPosition.java
@@ -0,0 +1,23 @@
+package org.ival.model;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.ival.model.base.CreatedBase;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "job_position")
+public class JobPosition extends CreatedBase {
+
+    @Id
+    @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
+    @GeneratedValue(generator = "uuid")
+    @Column(name = "id", nullable = false,length = 36)
+    private String id;
+
+    @Column(name = "title", length = 30, nullable = false)
+    private String title;
+
+    @Column(name = "salary", nullable = false)
+    private Double salary;
+}
